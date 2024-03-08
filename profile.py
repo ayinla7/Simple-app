e8ca99bebbba7f386da3a3cbbaaf6e3f42fc20bf
KWAMDEEN AYINLA <31147728+ayinla7@users.noreply.github.com>
2024-03-08 18:29:55 +0000

    Created Profile.py using streamlit

---
+++ profile.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.text("Get to know me... welcome to my profile, Amigo!")
+#st.title("KWAMDEEN AYINLA")
+st.markdown("<h1 style='text-align: center;'>KWAMDEEN AYINLA</h1>", unsafe_allow_html=True)
+st.image("./photo.jpg")
+st.header("Software Developer.")
+st.subheader("Father of 3, soon.")
+st.text("That's all for now, Amigo!")
